f936a38c8afe9a2bfd23cf08d3b6f8295310e1d0
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-11-30 11:36:22 -0800

    inital starter code

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
